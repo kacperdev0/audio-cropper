729b3977bfe1afeddf5ab6a5c1bac5949c7cd4f0
KacperOldz <kobula@protonmail.com>
2024-06-08 17:40:17 +0200

    updated function asking for permissions

--- app/src/main/java/com/example/audiocropper/MainActivity.kt
+++ app/src/main/java/com/example/audiocropper/MainActivity.kt
@@ -3,6 +3,9 @@ package com.example.audiocropper
 import android.Manifest
 import android.content.Intent
 import android.content.pm.PackageManager
+import android.media.MediaCodec
+import android.media.MediaExtractor
+import android.media.MediaMuxer
 import android.net.Uri
 import android.os.Bundle
 import android.widget.Button
@@ -14,15 +17,23 @@ import androidx.core.content.ContextCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import android.media.MediaPlayer
+import android.os.Environment
 import android.os.Handler
 import android.os.Looper
 import com.google.android.material.slider.RangeSlider
+import java.io.File
+import java.nio.ByteBuffer
+import com.mpatric.mp3agic.Mp3File
+import com.mpatric.mp3agic.InvalidDataException
+import com.mpatric.mp3agic.UnsupportedTagException
+import java.io.FileOutputStream
+import java.io.IOException
 
 
 class MainActivity : AppCompatActivity() {
     var PICK_FILE_REQ_CODE = 1
 
-    lateinit var urlDisplay: TextView
+    lateinit var url: Uri
     lateinit var mediaPlayer: MediaPlayer
     lateinit var timeSlider: RangeSlider
 
@@ -39,11 +50,17 @@ class MainActivity : AppCompatActivity() {
         mediaPlayer = MediaPlayer()
 
         findViewById<Button>(R.id.selectFileButton).setOnClickListener {
-            checkPermissionAndSelectFile()
+            if (checkPermissionAndSelectFile()) {
+                audioFilePicker()
+            }
         }
 
         findViewById<Button>(R.id.playButton).setOnClickListener {
             playPreview()
+        }
+
+        findViewById<Button>(R.id.saveButton).setOnClickListener {
+
         }
 
         timeSlider = findViewById<RangeSlider>(R.id.timeSlider)
@@ -74,6 +91,7 @@ class MainActivity : AppCompatActivity() {
 
         val selectedUri: Uri? = data?.data
         selectedUri?.let {
+            url = selectedUri
             mediaPlayer.setDataSource(applicationContext, selectedUri)
             mediaPlayer.prepare()
             timeSlider.valueTo = (mediaPlayer.duration / 1000).toFloat()
@@ -96,11 +114,15 @@ class MainActivity : AppCompatActivity() {
         }, (((timeSlider.values[1].toInt() * 1000) - startSec).toLong()))
     }
 
-    private fun checkPermissionAndSelectFile() {
-        if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+    private fun checkPermissionAndSelectFile(): Boolean {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED &&
+            ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
             ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE), 1)
+            ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.WRITE_EXTERNAL_STORAGE), 2)
+            return false
         } else {
-            audioFilePicker()
+            return true
         }
     }
 }
+
